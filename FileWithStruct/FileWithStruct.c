12e18ce5ef16aa849e39943ddd33d7a0350fd200
Mert Aksoy <50530097+mertaksoyy@users.noreply.github.com>
2022-03-24 23:32:27 +0300

    Add files via upload

---
+++ FileWithStruct/FileWithStruct.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+
+struct Movie{
+	char name[50];
+	int year;
+	float imdb;
+};
+
+
+
+int main()
+{
+	FILE *fp;
+	struct Movie movie[50];
+	
+	int i=0,a;
+	float maximdb=0.0;
+	
+	if((fp=fopen("Source.txt","r"))==NULL)
+	{
+		printf("Filed can't opened....'");
+		return 0;
+	}
+	
+	
+	while(!feof(fp))
+	{
+		fscanf(fp,"%s %d %f",movie[i].name,&movie[i].year,&movie[i].imdb);
+		
+		if(movie[i].imdb>maximdb)
+		{
+			a=i;
+			maximdb=movie[i].imdb;
+		}
+		i++;
+	}
+	
+	printf("\n %s %d %f",movie[a].name,movie[a].year,movie[a].imdb);
+	
+	fclose(fp);
+	return 0;
+	
+	
+	
+}
